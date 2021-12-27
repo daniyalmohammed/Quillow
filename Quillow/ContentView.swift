e34c6e689a82bdb042188f64be03267ef2e97137
daniyalmohammed <d25moham@uwaterloo.ca>
2021-12-27 15:32:18 -0500

    Initial Commit

---
+++ Quillow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quillow
+//
+//  Created by Daniyal Mohammed on 2021-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quillow/QuillowApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuillowApp.swift
+//  Quillow
+//
+//  Created by Daniyal Mohammed on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct QuillowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
